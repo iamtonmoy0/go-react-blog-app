e3f923b370c9c3eec97c9ff513c37cd204dafec6
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-29 00:07:00 +0600

    Add go-fiber as a dependency, initialize database connection and start a simple server
    The diff adds the go-fiber package as a dependency, initializes a connection to the database, checks the connection by performing a simple query, starts a new server on port 3000, and returns "Hello World!" when the root endpoint is accessed.

--- server/main.go
+++ server/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/iamtonmoy0/go-react-blog-app/database"
+)
+
 func main() {
+	db := database.Database()
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	defer sqlDB.Close()
+	// Check if the connection is successful by performing a simple query
+	var result int
+	db.Raw("SELECT 1").Scan(&result)
+	fmt.Println("Database connection successful!")
+
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+	// Start the server
+	if err := app.Listen(":3000"); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Server started on port 3000")
+	}
 
 }
